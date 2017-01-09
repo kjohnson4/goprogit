8588025313b0e8a4865ac7aca16cdbdf2d4e1312
kjohnson4 <kjohnhon4@gmail.com>
2017-01-09 16:50:39 -0600

    Add files via upload

---
+++ transmart-R-API.R
@@ -0,0 +1,15 @@
+# Initial Requirements
+require(devtools)
+install_github('transmart/RInterface')
+require("transmartRClient")
+# Request connection to tranSMART VM
+# This will return a URL you need to copy/paste into browser, then login to tranSMART, 
+#   authorize the token, then copy it and paste back here in the console
+connectToTransmart("URL_GOES_HERE")
+# Get list of studies
+studies <- getStudies()
+print(studies)
+# Assign a study and view further data
+study <- "STUDY_NUMBER_GOES_HERE"
+allObservations <- getObservations(study, as.data.frame = T)
+summary(allObservations)
